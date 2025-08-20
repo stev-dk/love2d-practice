fa0d935925e52e7c0d9af38456c97e08a3dc50a3
Steffan <steffan@mail.com>
2025-08-20 20:59:21 +0200

    Added sprites and random colours

--- falling-block-counter/block.lua
+++ falling-block-counter/block.lua
@@ -1,10 +1,13 @@
 Block = {}
 Block.__index = Block
 
-local GRAVITY = 450
+local GRAVITY = 128
 local SCREEN_WIDTH = 800
 local WIDTH = 1
 local HEIGHT = 1
+local RGB_MIN = 50 -- higher value will results in more vibrant colours
+local RGB_MAX = 255
+
 local blockSprites = {}
 
 local function randomXPos(spriteWidth)
@@ -16,18 +19,25 @@ local function randomSprite()
     return string.format("assets/%i.png", i)
 end
 
+local function randomColors()
+    return {math.random(RGB_MIN,RGB_MAX), math.random(RGB_MIN,RGB_MAX), math.random(RGB_MIN,RGB_MAX)}
+end
+
 function Block:new(o)
     o = o or {}
-    o.yPosition = 0
     o.counter = o.counter
     o.SCREEN_HEIGHT = o.SCREEN_HEIGHT
     o.sprite = o.sprite or randomSprite()
+    o.colors = randomColors()
 
     if not blockSprites[o.sprite] then
         blockSprites[o.sprite] = love.graphics.newImage(o.sprite)
     end
 
+    o.spriteHeight = -blockSprites[o.sprite]:getHeight()
     o.spriteWidth = blockSprites[o.sprite]:getWidth()
+
+    o.yPosition = o.spriteHeight
     o.xPosition = randomXPos(o.spriteWidth)
 
     setmetatable(o, self)
@@ -42,7 +52,12 @@ function Block:update(dt)
 end
 
 function Block:draw()
+    love.graphics.setColor(love.math.colorFromBytes(self.colors))
     love.graphics.draw(blockSprites[self.sprite], self.xPosition, self.yPosition, math.deg(0), WIDTH, HEIGHT)
+
+    -- DEBUG
+    love.graphics.print(string.format('R: %i, G: %i, B: %i', self.colors[1], self.colors[2], self.colors[3]), self.xPosition, self.yPosition - 32)
+    -- DEBUG
 end
 
 return Block
